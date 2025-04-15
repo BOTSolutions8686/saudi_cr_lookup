a471f777b06f82f287d84bffa2c741cea104d172
BOTSolutions8686 <101816406+BOTSolutions8686@users.noreply.github.com>
2025-04-15 18:27:48 +0500

    Delete saudi_cr_lookup directory

--- saudi_cr_lookup/saudi_cr_lookup/saudi_cr_lookup/doctype/saudi_cr_lookup_settings/test_saudi_cr_lookup_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, BOT SOlutions and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSaudiCRLookupSettings(FrappeTestCase):
-	pass
